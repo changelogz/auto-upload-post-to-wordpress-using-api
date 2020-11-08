ec8ec9d8fdd229b75682b769b8912d33b95577f7
unknown <nghiahsgs@gmail.com>
2020-11-09 06:19:19 +0700

    make it better

--- index.py
+++ index.py
@@ -1,15 +1,26 @@
+# plug in 
+# https://github.com/WP-API/Basic-Auth
+# chay xong tool thi xoa plugin + doi mat khau
+
 # https://developer.wordpress.org/rest-api/
 # https://pypi.org/project/wordpress-api/
 
 from high_order_framework_requests_python import utils_class
 import requests
 
+
+
 import base64
 def base_64_encode(message):
     # message = "admin:password"
     message_bytes = message.encode('ascii')
     base64_bytes = base64.b64encode(message_bytes)
-    return '%s'%base64_bytes
+    base64_string = base64_bytes.decode("ascii") 
+    return base64_string
+
+message = 'nghiahsgs:261997'
+print(base_64_encode(message))
+
 
 username = 'nghiahsgs'
 password = '261997'
@@ -17,10 +28,13 @@ headers = {
     'Authorization':'Basic %s'%base_64_encode('%s:%s'%(username,password))
 }
 
+
+
+
 url = 'http://nghiahsgs.com/wp-json/wp/v2/posts'
 data = {
     'title'   : 'My test',
-    'status'  : 'draft', # ok, we do not want to publish it immediately
+    # 'status'  : 'draft', # ok, we do not want to publish it immediately
     'content' : 'lalala',
     'categories' : 5, # category ID
     'tags' : '1,4,23', # string, comma separated
@@ -33,3 +47,5 @@ data = {
 }
 res = requests.post(url, data=data,headers=headers)
 print(res)
+
+utils_class.File_Interact('res.txt').write_file(res.text)
